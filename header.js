6bcc284dc45c1769f7ea06e0bec1c3aead2669ea
ujin5088 <127194487+ujin5088@users.noreply.github.com>
2023-06-14 19:48:30 +0900

    Delete header.js

--- header.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.nav-toggleBtn');
-const menu = document.querySelector('.menu');
-
-toggleBtn.addEventListener('click', () => {
-menu.classList.toggle('active');
-});
